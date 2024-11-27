8b394661a7244d2ebaf13ea9e71b6d50265ad10d
truptikatkar <trupti.katkar@g99.plus>
2024-11-27 14:41:32 +0530

    subscription code

--- src/app/pages/subscriptions/subscriptions.component.ts
+++ src/app/pages/subscriptions/subscriptions.component.ts
@@ -1,12 +1,16 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { UntypedFormBuilder, Validators } from '@angular/forms';
+import { injectStripe, StripePaymentElementComponent } from 'ngx-stripe';
+import { StripeElementsOptions, StripePaymentElementOptions } from '@stripe/stripe-js';
+import { environment } from '../../../environments/environment.development';
 
 @Component({
   selector: 'app-subscriptions',
   templateUrl: './subscriptions.component.html',
   styleUrl: './subscriptions.component.scss'
 })
-export class SubscriptionsComponent {
+export class SubscriptionsComponent implements OnInit, AfterViewInit {
   planBenifits = [
     "View unlimited photos",
     "Unlimited messaging",
@@ -17,6 +21,139 @@ export class SubscriptionsComponent {
 
   authService = inject(AuthService)
 
+  @ViewChild(StripePaymentElementComponent)
+  paymentElement!: StripePaymentElementComponent;
+
+  private readonly fb = inject(UntypedFormBuilder);
+
+  paymentElementForm = this.fb.group({
+    name: ['John Doe', [Validators.required]],
+    email: ['support@ngx-stripe.dev', [Validators.required]],
+    address: [''],
+    zipcode: [''],
+    city: [''],
+    amount: [2500, [Validators.required, Validators.pattern(/\d+/)]]
+  });
+
+  elementsOptions: any = {
+    locale: 'en',
+    client: environment.stripeSecreteKey,
+    appearance: {
+      theme: 'flat'
+    }
+  };
+
+  paymentElementOptions: StripePaymentElementOptions = {
+    layout: {
+      type: 'tabs',
+      defaultCollapsed: false,
+      radios: false,
+      spacedAccordionItems: false
+    }
+  };
+
+  // Replace with your own public key
+  stripe = injectStripe(environment.stripePublicKey);
+  paying = signal(false);
+
+  paymentHandler: any = null;
+
+  ngAfterViewInit() {
+    // Check if paymentElement is initialized properly
+    if (!this.paymentElement) {
+      console.error('Payment Element is not initialized');
+    }
+  }
+
+  ngOnInit() {
+    this.invokeStripe();
+    if (!this.stripe) {
+      console.error('Stripe is not initialized');
+    }
+  }
+
+  invokeStripe() {
+    if (!window.document.getElementById('stripe-script')) {
+      const script = window.document.createElement('script');
+
+      script.id = 'stripe-script';
+      script.type = 'text/javascript';
+      script.src = 'https://checkout.stripe.com/checkout.js';
+      script.onload = () => {
+        this.paymentHandler = (<any>window).StripeCheckout.configure({
+          key: environment.stripePublicKey,
+          locale: 'auto',
+          token: function (stripeToken: any) {
+            console.log(stripeToken);
+            alert('Payment has been successfull!');
+          },
+        });
+      };
+
+      window.document.body.appendChild(script);
+    }
+  }
+
+  makePayment(amount: any) {
+    const paymentHandler = (<any>window).StripeCheckout.configure({
+      key: environment.stripePublicKey,
+      locale: 'auto',
+      token: function (stripeToken: any) {
+        console.log(stripeToken);
+        alert('Stripe token generated!');
+      },
+    });
+    paymentHandler.open({
+      name: 'DatingApp.com',
+      description: 'find your love',
+      amount: amount * 100,
+    });
+  }
+
+  pay() {
+    if (this.paying() || this.paymentElementForm.invalid) return;
+    this.paying.set(true);
+
+    const {
+      name,
+      email,
+      address,
+      zipcode,
+      city
+    } = this.paymentElementForm.getRawValue();
+
+    this.stripe
+      .confirmPayment({
+        elements: this.paymentElement.elements,
+        confirmParams: {
+          payment_method_data: {
+            billing_details: {
+              name: name as string,
+              email: email as string,
+              address: {
+                line1: address as string,
+                postal_code: zipcode as string,
+                city: city as string
+              }
+            }
+          }
+        },
+        redirect: 'if_required'
+      })
+      .subscribe((result: any) => {
+        this.paying.set(false);
+        if (result.error) {
+          // Show error to your customer (e.g., insufficient funds)
+          alert({ success: false, error: result.error.message });
+        } else {
+          // The payment has been processed!
+          if (result.paymentIntent.status === 'succeeded') {
+            // Show a success message to your customer
+            alert({ success: true });
+          }
+        }
+      });
+  }
   handleLogin() {
     this.authService.logout();
   }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    // apiUrl: 'http://localhost:5000/'
-    apiUrl: 'https://backend-dating.onrender.com/'
+    apiUrl: 'http://localhost:5000/',
+    // apiUrl: 'https://backend-dating.onrender.com/',
+    stripePublicKey: "pk_test_51QP14jGK2i8XdRUuUeVGDq6rmdGdrCY9SridenaBGwawj0jWAN0dKMFDTaimGdQS4xEAgGv9jazSCSbI2hK8IGSh00cs1oVSxg",
+    stripeSecreteKey: "sk_test_51QP14jGK2i8XdRUuEwbUTRaWEgZFfL1zbCzyvMSOSUN0iIK05IXAVXrnS6N5waQjbL7kAiaMEB31efAIC7Kj1j2f00gp2YV0Ts"
+
 };
